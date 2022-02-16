2192a847f7b6e8c9c45884f0eae196114dfb8468
Loeini <loeini.pucca@gmail.com>
2022-02-16 16:19:44 -0300

    questão 01

---
+++ questao01.py
@@ -0,0 +1,12 @@
+"""
+Quastão 01
+Escreva um algoritmo que mostre na tela uma escada de tamanho n utilizando o caractere * e espaços.
+A base e altura da escada devem ser iguais ao valor de n. A última linha não deve conter nenhum espaço.
+"""
+
+tamanho = int(input('Insira um número inteiro:\n'))
+quantidade = 1
+
+while tamanho >= quantidade:
+   print(f'{"*"*quantidade :>{tamanho}}')
+   quantidade = quantidade +1
